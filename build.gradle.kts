baf876c19367bee1553674d6fad4f093910cbeb3
lolmageap <ekxk1234@naver.com>
2024-02-07 22:30:26 +0900

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.22"
-    id("io.ktor.plugin") version "2.3.7"
+    kotlin("jvm") version "1.9.21"
+    id("io.ktor.plugin") version "2.3.8"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
 }
 
 group = "com.example"
@@ -22,12 +24,28 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-jackson-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("org.apache.kafka:kafka-streams:3.6.1")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("org.jasypt:jasypt:1.9.3")
+
+    implementation("org.litote.kmongo:kmongo:4.11.0")
+    implementation("com.slack.api:slack-api-client:1.38.0")
+
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks.register("databaseInstance") {
+    doLast {
+        val command = arrayOf("docker-compose", "up")
+        Runtime.getRuntime().exec(command)
+    }
 }
