f175a14fa4779d1f657ea6759a8fc2fba1625dda
lolmageap <ekxk1234@naver.com>
2024-02-09 22:04:49 +0900

    add enable scheduling

--- src/main/kotlin/com/example/filialscheduler/FilialSchedulerApplication.kt
+++ src/main/kotlin/com/example/filialscheduler/FilialSchedulerApplication.kt
@@ -2,7 +2,9 @@ package com.example.filialscheduler
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class FilialSchedulerApplication
 
