90304887ccd71a8f640951491d60e545574b3daa
lolmageap <ekxk1234@naver.com>
2024-02-10 23:55:38 +0900

    create send to slack

---
+++ src/main/kotlin/com/example/filialscheduler/client/SlackClient.kt
@@ -0,0 +1,41 @@
+package com.example.filialscheduler.client
+
+import com.example.filialscheduler.property.SlackProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+
+
+@Component
+@EnableConfigurationProperties(SlackProperty::class)
+class SlackClient(
+    private val slackProperty: SlackProperty,
+    private val objectMapper: ObjectMapper,
+) {
+
+    suspend fun sendMessage(): Unit = coroutineScope {
+        val webClient = WebClient.create(slackProperty.url)
+        val slackMessage = mapOf("text" to "message 보냄")
+
+        val value = BodyInserters.fromValue(
+            objectMapper.writeValueAsString(slackMessage)
+        )
+
+        try {
+            launch {
+                webClient.post()
+                    .body(value)
+                    .retrieve()
+                    .awaitBody()
+            }
+        } catch (e: Exception) {
+            TODO("Exception handling")
+        }
+    }
+
+}
