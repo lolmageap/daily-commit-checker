fca38e51ce81cb9fa88672462425ead29e8ce4cc
lolmageap <ekxk1234@naver.com>
2024-02-10 23:56:00 +0900

    add send message logic

--- src/main/kotlin/com/example/filialscheduler/scheduler/Scheduler.kt
+++ src/main/kotlin/com/example/filialscheduler/scheduler/Scheduler.kt
@@ -1,16 +1,26 @@
 package com.example.filialscheduler.scheduler
 
 import com.example.filialscheduler.client.GithubClient
+import com.example.filialscheduler.client.SlackClient
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class Scheduler(
     private val githubClient: GithubClient,
+    private val slackClient: SlackClient,
 ) {
 
     @Scheduled(cron = "0 1 0 * * *", zone = "Asia/Seoul")
     suspend fun schedule() {
-        githubClient.getCommitsCountForYesterday()
+        val count = githubClient.getCommitsCountForYesterday()
+
+        if (count == 0) {
+            try {
+                slackClient.sendMessage()
+            } catch (e: Exception) {
+                TODO("error handling")
+            }
+        }
     }
 }
