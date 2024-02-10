2100e0b604723894a8debc77f7ce691cec69e60e
lolmageap <ekxk1234@naver.com>
2024-02-10 23:54:53 +0900

    add slack test end point

--- src/main/kotlin/com/example/filialscheduler/TestController.kt
+++ src/main/kotlin/com/example/filialscheduler/TestController.kt
@@ -1,15 +1,20 @@
 package com.example.filialscheduler
 
 import com.example.filialscheduler.client.GithubClient
+import com.example.filialscheduler.client.SlackClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController(
     private val githubClient: GithubClient,
+    private val slackClient: SlackClient,
 ) {
 
     @GetMapping("/test")
     suspend fun test() = githubClient.getCommitsCountForYesterday()
+
+    @GetMapping("/slack")
+    suspend fun slackTest() = slackClient.sendMessage()
 
 }
