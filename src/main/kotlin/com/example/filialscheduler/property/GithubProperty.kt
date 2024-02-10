e9e1c609478477692ef9905738d6d15e2946b24b
lolmageap <ekxk1234@naver.com>
2024-02-10 23:53:59 +0900

    fix to data class

--- src/main/kotlin/com/example/filialscheduler/property/GithubProperty.kt
+++ src/main/kotlin/com/example/filialscheduler/property/GithubProperty.kt
@@ -3,7 +3,7 @@ package com.example.filialscheduler.property
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "github")
-class GithubProperty {
-    lateinit var user: String
-    lateinit var token: String
-}
+data class GithubProperty(
+    val user: String,
+    val token: String,
+)
