ed1291dd270e59ab69b5496d8a1640e19efde71b
lolmageap <ekxk1234@naver.com>
2024-02-11 23:01:52 +0900

    delete toss

--- src/main/kotlin/com/example/filialscheduler/property/TossProperty.kt
+++
@@ -1,9 +0,0 @@
-package com.example.filialscheduler.property
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "toss")
-data class TossProperty(
-    val clientKey: String,
-    val secretKey: String,
-)
