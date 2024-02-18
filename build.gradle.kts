c1fc2f0ccaa0a662f3fbe66a7d364733325c038a
lolmageap <ekxk1234@naver.com>
2024-02-18 17:41:30 +0900

    add kotest dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.5.2")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-//    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 
     implementation("org.jasypt:jasypt:1.9.3")
@@ -34,6 +33,10 @@ dependencies {
     implementation("net.nurigo:sdk:4.3.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 }
 
 tasks.withType<KotlinCompile> {
