764974d1ed72f6cba393241c039050d384c20724
lolmageap <ekxk1234@naver.com>
2024-03-08 23:33:47 +0900

    remove configuration properties

--- src/main/kotlin/com/example/filialscheduler/property/CoolsmsProperty.kt
+++ src/main/kotlin/com/example/filialscheduler/property/CoolsmsProperty.kt
@@ -1,8 +1,5 @@
 package com.example.filialscheduler.property
 
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "coolsms")
 data class CoolsmsProperty(
     val apiKey: String,
     val apiSecret: String,
--- src/main/kotlin/com/example/filialscheduler/property/GithubProperty.kt
+++ src/main/kotlin/com/example/filialscheduler/property/GithubProperty.kt
@@ -1,8 +1,5 @@
 package com.example.filialscheduler.property
 
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "github")
 data class GithubProperty(
     val user: String,
     val token: String,
