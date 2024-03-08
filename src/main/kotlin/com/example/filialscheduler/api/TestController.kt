44f692fc88ad7bcfc3740f7efd7100001b455540
lolmageap <ekxk1234@naver.com>
2024-03-08 23:34:08 +0900

    add slack success and failure test

--- src/main/kotlin/com/example/filialscheduler/api/TestController.kt
+++ src/main/kotlin/com/example/filialscheduler/api/TestController.kt
@@ -3,6 +3,9 @@ package com.example.filialscheduler.api
 import com.example.filialscheduler.client.GithubClient
 import com.example.filialscheduler.client.SlackClient
 import com.example.filialscheduler.client.SmsClient
+import com.example.filialscheduler.extension.defaultSerializeSuccessMessage
+import com.example.filialscheduler.extension.defaultSerializedFailureMessage
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,13 +15,21 @@ class TestController(
     private val githubClient: GithubClient,
     private val slackClient: SlackClient,
     private val smsClient: SmsClient,
+    private val objectMapper: ObjectMapper,
 ) {
 
     @GetMapping("/github")
     suspend fun github() = githubClient.getCommitsCountForYesterday()
 
-    @PostMapping("/slack")
-    suspend fun slackTest() = slackClient.sendErrorMessage()
+    @PostMapping("/slack/error")
+    suspend fun slackFailureTest() = slackClient.sendMessage(
+        objectMapper.defaultSerializedFailureMessage
+    )
+
+    @PostMapping("/slack/success")
+    suspend fun slackSuccessTest() = slackClient.sendMessage(
+        objectMapper.defaultSerializeSuccessMessage
+    )
 
     @PostMapping("/sms")
     suspend fun smsTest() = smsClient.sendSms()
