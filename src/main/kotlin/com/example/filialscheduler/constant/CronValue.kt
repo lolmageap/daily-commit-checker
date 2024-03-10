fc876d61d39e81939b9cb334380ce0c640a30387
lolmageap <ekxk1234@naver.com>
2024-03-10 22:40:45 +0900

    add MONDAY_ONE_MINUTE_PAST_TWELVE_PM

--- src/main/kotlin/com/example/filialscheduler/constant/CronValue.kt
+++ src/main/kotlin/com/example/filialscheduler/constant/CronValue.kt
@@ -1,5 +1,6 @@
 package com.example.filialscheduler.constant
 
 
-const val ONE_MINUTE_PAST_TWELVE_PM = "0 1 0 * * *"
+const val EVERYDAY_ONE_MINUTE_PAST_TWELVE_PM = "0 1 0 * * *"
+const val MONDAY_ONE_MINUTE_PAST_TWELVE_PM = "0 1 0 * * MON"
 const val ASIA_SEOUL = "Asia/Seoul"
