f4d37cebabddeaf6d222fdc8751803785dce482e
lolmageap <ekxk1234@naver.com>
2024-03-10 22:39:53 +0900

    create file component

---
+++ src/main/kotlin/com/example/filialscheduler/component/FileComponent.kt
@@ -0,0 +1,27 @@
+package com.example.filialscheduler.component
+
+import com.example.filialscheduler.property.LocationProperty
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.stereotype.Component
+import java.io.File
+
+@Component
+@EnableConfigurationProperties(LocationProperty::class)
+class FileComponent(
+    private val locationProperty: LocationProperty,
+) {
+    suspend fun readLastBlogId(): String = withContext(Dispatchers.IO) {
+        val file = File(locationProperty.blogPath)
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        file.readText()
+    }
+
+    suspend fun writeLastBlogId(id: String) = withContext(Dispatchers.IO) {
+        val file = File(locationProperty.blogPath)
+        file.writeText(id)
+    }
+}
