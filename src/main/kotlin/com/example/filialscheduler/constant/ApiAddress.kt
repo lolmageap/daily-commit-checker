286cdc7538b423d8b2fe0bfe94345669639f3465
lolmageap <ekxk1234@naver.com>
2024-03-25 22:46:26 +0900

    add api address to constant

---
+++ src/main/kotlin/com/example/filialscheduler/constant/ApiAddress.kt
@@ -0,0 +1,7 @@
+package com.example.filialscheduler.constant
+
+const val COOL_SMS_ENDPOINT = "https://api.coolsms.co.kr"
+
+const val GITHUB_ENDPOINT = "https://api.github.com"
+
+const val VELOG_ENDPOINT = "https://velog.io"
