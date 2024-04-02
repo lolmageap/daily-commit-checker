f338720db976d6a0ca2285bf0543ea3cfc3adfc9
lolmageap <ekxk1234@naver.com>
2024-04-02 21:49:36 +0900

    add cron value

--- src/main/kotlin/com/example/filialscheduler/constant/CronValue.kt
+++ src/main/kotlin/com/example/filialscheduler/constant/CronValue.kt
@@ -3,4 +3,5 @@ package com.example.filialscheduler.constant
 
 const val EVERYDAY_ONE_MINUTE_PAST_TWELVE_PM = "0 1 0 * * *"
 const val FIRST_DAY_ONE_MINUTE_PAST_MIDNIGHT = "0 1 0 1 * ?"
+const val EVERY_DAY_EIGHT_THIRTY_AM = "0 30 8 * * *"
 const val ASIA_SEOUL = "Asia/Seoul"
