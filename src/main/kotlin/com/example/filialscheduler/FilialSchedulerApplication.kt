6203c2582df1ffd35b0dbed482f96af74979b986
lolmageap <ekxk1234@naver.com>
2024-04-02 21:48:38 +0900

    add enable configuration property scan

--- src/main/kotlin/com/example/filialscheduler/FilialSchedulerApplication.kt
+++ src/main/kotlin/com/example/filialscheduler/FilialSchedulerApplication.kt
@@ -1,10 +1,12 @@
 package com.example.filialscheduler
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
+@ConfigurationPropertiesScan("com.example.filialscheduler.property")
 @SpringBootApplication
 class FilialSchedulerApplication
 
